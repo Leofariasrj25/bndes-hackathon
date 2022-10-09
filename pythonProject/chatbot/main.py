bbf64c954834d686a327cf84767a7006ae3a3280
Mateus Pinna Lousa <mateuslousa@gmail.com>
2022-10-09 18:23:47 -0300

    Bot testado

--- pythonProject/chatbot/main.py
+++ pythonProject/chatbot/main.py
@@ -1,37 +1,48 @@
-import keys
+import os
 from telegram.ext import *
 from telegram import Update
+import json
 import responses as r
 
 print("Bot started...")
 
 data = dict()
+
+# pega o id do usuário que esta se comunicando com o bot no momento
 def get_id(update):
 	return update.message.chat.id
 
+# inicializa dicionario com as categorias das respostas
 def create_fields(id):
 	global data
-	data[id] = {'nome': '', 'cpf': '', 'estado' : '', 'renda' : '', 'maturidade' : False}
+	data[id] = {'pergunta' : 0, 'nome': '', 'CNPJ': '', 'estado' : '', 'renda' : '', 'credito' : '', 'maturidade' : False}
 
+# quando o comando /start é solicitado
 def start_command(update, context):
-	update.message.reply_text("Olá! Fico muito feliz em ver que você quer investir no seu negócio. Se você deseja se cadastrar digite 'cadastrar' para iniciar o processo (utilize dados fictícios para testar o bot 😊)")
+	global data
+	update.message.reply_text("Olá! Fico muito feliz em ver que você quer investir no seu negócio. Se você deseja se cadastrar digite 'cadastro' para iniciar o processo (utilize dados fictícios para testar o bot 😊)")
 	create_fields(get_id(update))
-	print(data)
 
+# quando o comando /hellp é solicitado
 def help_command(update, context):
-	update.message.reply_text("Por enquanto eu sou só um protótipo e só cadastro as pessoas. Digite 'cadastrar' para iniciar o processo de cadastro")
+	update.message.reply_text("Por enquanto eu sou só um protótipo e só cadastro as pessoas. Digite 'cadastro' para iniciar o processo de cadastro")
 
+# Função para lidar com as diferentes mensagens recebidas
 def handle_message(update, context):
 	text = str(update.message.text).lower()
-	response = r.sample_responses(text)
+	response = r.sample_responses(text, data[get_id(update)])
 	update.message.reply_text(response)
 	print(update.message.chat.id)
+	print(data[get_id(update)]['pergunta'])
+	if data[get_id(update)]['pergunta'] == -1:
+		update.message.reply_text(json.dumps(data[get_id(update)]))
 
+# printa o erro no terminal e seu contexto no terminal
 def error(update, context):
 	print(f"Update {update} caused error {context.error}")
 
 def main():
-	updater = Updater(keys.API_KEY, use_context = True)
+	updater = Updater(os.environ['API_KEY'], use_context = True)
 	dp = updater.dispatcher
 	dp.add_handler(CommandHandler("start", start_command))
 	dp.add_handler(CommandHandler("help", help_command))
--- pythonProject/chatbot/responses.py
+++ pythonProject/chatbot/responses.py
@@ -1,10 +1,71 @@
+from telegram.ext import *
 
+# Verificação de digitos
+def has_numbers(inputString):
+    return any(char.isdigit() for char in inputString)
 
-def sample_responses(input_text):
+# Verificação de letras
+def has_alpha(inputString):
+    return any(char.alpha() for char in inputString)
+
+# Logica responsável por selecionar as respostas. Ela toma como input
+# a resposta do usuário e verifica se não tem nenhum erro grotesco.
+# Caso esteja tudo ok, o usuário recebe a próxima pergunta, caso contrário,
+# uma mensagem de erro é exibida até que ele coloque uma resposta adequada.
+def sample_responses(input_text, data):
 	user_message = str(input_text).lower()
 
+
+	if "cadastr" in user_message:
+		data['pergunta'] = 1
+		return "Vamos iniciar o seu cadastro. Qual é o seu nome?"
+	elif data['pergunta'] == 1:
+		if has_numbers(user_message):
+			data['pergunta'] = 1
+			return "Numeros não sao permitidos! Tente novamente."
+		else:
+			data.update({'nome':user_message})
+			data['pergunta'] += 1
+			return "Digite seu CNPJ (Sem pontos ou traços)"
+	elif data['pergunta'] == 2:
+		if user_message.isdigit() is False:
+			data['pergunta'] = 2
+			return "Seu CNPJ deve conter apenas digitos! Tente novamente."
+		else:
+			data.update({'CNPJ': user_message})
+			data['pergunta'] += 1
+			return "Qual estado(Sigla) você reside: Ex: RJ, CE, SC ..."
+	elif data['pergunta'] == 3:
+		if user_message in ("rj, sp, es, mg, pr, sc, rs, ms, go, ac, al, ap, am, ba, ce, df, ma, mt, pa, pb, pe, pi, rn, ro, rr ,se, to"):
+			data.update({'estado': user_message})
+			data['pergunta'] += 1
+			return "Qual sua renda bruta mensal em reais? Ex: 1000"
+		else:
+			data['pergunta'] == 3
+			return "Digite uma Sigla Válida"
+	elif data['pergunta'] == 4:
+		if user_message.isdigit() is False:
+			data['pergunta'] = 4
+			return "Esse valor deve conter apenas digitos! Tente novamente."
+		else:
+			data.update({'renda': user_message})
+			data['pergunta'] += 1
+			return "Quando é o valor do crédito que você procura em reais? ex: 4000"
+	elif data['pergunta'] == 5:
+		if user_message.isdigit() is False:
+			data['pergunta'] = 5
+			return "Esse valor deve conter apenas digitos! Tente novamente."
+		else:
+			data.update({'credito': user_message})
+			data['pergunta'] += 1
+			return "Possui MEI a mais de 6 meses?"
+	elif data['pergunta'] == 6:
+		data['pergunta'] = -1
+		if user_message in ("sim", "s"):
+			data.update({'maturidade': True})
+		else:
+			data.update({'maturidade': False})
+		print(data)
+		return "Obrigado Pelas Respostas!"
 	if user_message in ("oi", "ola", "olá"):
 		return "Olááá!"
-	elif user_message in ("cadastro"):
-		return "Qual é o seu nome?"
-	
